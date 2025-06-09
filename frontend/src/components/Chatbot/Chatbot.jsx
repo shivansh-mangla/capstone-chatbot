ebffce627f01f043ee20826cc6a155aed774b19f
Shivansh <155874081+shivansh-mangla@users.noreply.github.com>
2025-06-09 20:39:27 +0530

    Added basic components

---
+++ frontend/src/components/Chatbot/Chatbot.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chatbot.css'
+
+const Chatbot = () => {
+  return (
+    <div className='chatbot'>
+      <h1>I AM A CHATBOT</h1>
+    </div>
+  )
+}
+
+export default Chatbot
